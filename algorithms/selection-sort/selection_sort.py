8b02b79a50c73c9defe36570d86c52ed21143d48
Anwaar Ahmed <anwaarahmedjunaid@gmail.com>
2026-07-21 17:19:31 +0500

    Improvements

--- algorithms/selection-sort/selection_sort.py
+++ algorithms/selection-sort/selection_sort.py
@@ -27,26 +27,8 @@ def selection_sort(items):
 
 
 if __name__ == '__main__':
-    # Test case 1: Unsorted list
-    test1 = [64, 34, 25, 12, 22, 11, 90]
-    print("Original list:", test1)
-    selection_sort(test1)
-    print("Sorted list:", test1)
-    
-    # Test case 2: Already sorted list
-    test2 = [1, 2, 3, 4, 5]
-    print("\nOriginal list:", test2)
-    selection_sort(test2)
-    print("Sorted list:", test2)
-    
-    # Test case 3: Reverse sorted list
-    test3 = [5, 4, 3, 2, 1]
-    print("\nOriginal list:", test3)
-    selection_sort(test3)
-    print("Sorted list:", test3)
-    
-    # Test case 4: List with duplicates
-    test4 = [3, 1, 4, 1, 5, 9, 2, 6]
-    print("\nOriginal list:", test4)
-    selection_sort(test4)
-    print("Sorted list:", test4)
+    # Test case
+    test = [64, 34, 25, 12, 22, 11, 90]
+    print("Original list:", test)
+    selection_sort(test)
+    print("Sorted list:", test)
